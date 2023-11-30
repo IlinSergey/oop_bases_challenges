35cc2ced957279b5bc2f12ea5536d0e471c68dfb
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-30 07:31:32 +0300

    a done

--- level_2/a_user_from_functions_to_class.py
+++ level_2/a_user_from_functions_to_class.py
@@ -6,13 +6,25 @@
 """
 
 
-def make_username_capitalized(username: str):
-    return username.capitalize()
-
-
-def generate_short_user_description(username: str, user_id: int, name: str):
-    return f'User with id {user_id} has {username} username and {name} name'
-
-
 class User:
-    pass  # код писать тут
+
+    def __init__(self, user_id: int, username: str, name: str) -> None:
+        self.user_id = user_id
+        self.username = username
+        self.name = name
+
+    def make_username_capitalized(self) -> str:
+        return self.username.capitalize()
+
+    def generate_short_user_description(self) -> str:
+        return f'User with id {self.user_id} has {self.username} username and {self.name} name'
+
+    def __repr__(self) -> str:
+        return f'user_id: {self.user_id}, user_name: {self.username}, name: {self.name}'
+
+
+if __name__ == '__main__':
+    user = User(user_id=1, username='Serg', name='Sergey')
+    print(user)
+    print(user.generate_short_user_description())
+    print(user.make_username_capitalized())
