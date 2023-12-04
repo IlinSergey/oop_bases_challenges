dcac2015e92a2c26d5aa23432c50f3fc3ee6b106
Sergey Ilin <ilin_sergey90@mail.ru>
2023-12-04 22:48:13 +0300

    b done

--- level_4/b_food_product.py
+++ level_4/b_food_product.py
@@ -14,20 +14,34 @@ from datetime import datetime
 
 
 class Product:
-    def __init__(self, title, quantity):
+    def __init__(self, title: str, quantity: int) -> None:
         self.title = title
         self.quantity = quantity
 
-    def get_full_info(self):
+    def get_full_info(self) -> str:
         return f'Product {self.title}, {self.quantity} in stock.'
 
-    def is_available(self):
+    def is_available(self) -> bool:
         return self.quantity > 0
 
 
 class FoodProduct(Product):
-    pass  # код писать тут
+    def __init__(self, title: str, quantity: int, expiration_date: str) -> None:
+        super().__init__(title, quantity)
+        self.expirated_date = expiration_date
+
+    def get_full_info(self) -> str:
+        return f'Product {self.title}, with expirated_date to {self.expirated_date}, {self.quantity} in stock.'
+
+    def is_available(self) -> bool:
+        expirated_date_datetime = datetime.strptime(self.expirated_date, '%d-%m-%Y')
+        return super().is_available() and expirated_date_datetime > datetime.now()
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    product = Product(title='Lemonade', quantity=10)
+    food_product = FoodProduct(title='Bread', quantity=5, expiration_date='05-12-2023')
+    assert product.get_full_info() == 'Product Lemonade, 10 in stock.'
+    assert product.is_available() is True
+    assert food_product.get_full_info() == 'Product Bread, with expirated_date to 05-12-2023, 5 in stock.'
+    assert food_product.is_available() is True  # on 04-12-2023
