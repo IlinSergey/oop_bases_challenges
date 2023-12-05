b06e554226a9e15c709a8fc9cced80fe547c18c7
Sergey Ilin <ilin_sergey90@mail.ru>
2023-12-05 19:20:24 +0300

    b done

--- level_5/b_change_admin_email.py
+++ level_5/b_change_admin_email.py
@@ -9,21 +9,24 @@
 
 
 class User:
-    def __init__(self, username: str, email: str):
+
+    def __init__(self, username: str, email: str) -> None:
         self.username = username
         self.email = email
 
-    def change_email(self, new_email):
+    def change_email(self, new_email: str) -> None:
         self.email = new_email
 
 
 class AdminUserMixin:
-    def change_email(self, new_email):
+
+    def change_email(self, new_email: str) -> SystemError:
         raise SystemError('It is impossible to change the email address of the administrator')
 
 
-class AdminUser(User, AdminUserMixin):
-    def change_user_info(self, user: User, new_username: str, new_email: str):
+class AdminUser(AdminUserMixin, User):  # type: ignore [misc]
+
+    def change_user_info(self, user: User, new_username: str, new_email: str) -> None:
         user.username = new_username
         user.email = new_email
 
@@ -31,5 +34,4 @@ class AdminUser(User, AdminUserMixin):
 if __name__ == '__main__':
     admin_user = AdminUser(username='python_dev', email='learnpython@yandex.ru')
     admin_user.change_email('new_email@gmail.com')
-    print(admin_user.email)
-
+    print(admin_user.email)  # noqa: T201
